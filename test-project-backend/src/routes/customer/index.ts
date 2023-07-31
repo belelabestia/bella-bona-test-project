626a58383b1e04b86e54114f294e96d6b947c94e
Marco Bellezza <jasone.mb@outlook.com>
2023-07-31 18:13:17 +0200

    add customer index endpoint

--- test-project-backend/src/routes/customer/index.ts
+++ test-project-backend/src/routes/customer/index.ts
@@ -3,6 +3,8 @@ import { FastifyPluginAsync, FastifyRequest } from "fastify";
 type Request = FastifyRequest<{ Params: { id: number } }>;
 
 const customer: FastifyPluginAsync = async (fastify): Promise<void> => {
+  fastify.get("/", () => fastify.prisma.customer.findMany());
+
   fastify.get("/:id", { schema: { params: { id: { type: "number" } } } }, async (request: Request, reply) => {
     const { id } = request.params;
     const customer = await fastify.prisma.customer.findUnique({ where: { id } });

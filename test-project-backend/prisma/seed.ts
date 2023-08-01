55126a5be79ba1d034741c471005f83eb1e352a8
Marco Bellezza <jasone.mb@outlook.com>
2023-08-01 10:45:36 +0200

    add product orders seed

--- test-project-backend/prisma/seed.ts
+++ test-project-backend/prisma/seed.ts
@@ -85,6 +85,191 @@ const seedOrders = () => prisma.order.createMany({
   ]
 });
 
+const seedProductOrders = () => prisma.productOrder.createMany({
+  data: [
+    {
+      id: 1,
+      name: "Product 1 order 1",
+      productId: 1,
+      quantity: 2,
+      status: "processing",
+      orderId: 1,
+      orderDate: new Date("2020-01-01")
+    },
+    {
+      id: 2,
+      name: "Product 1 order 1",
+      productId: 1,
+      quantity: 5,
+      status: "processing",
+      orderId: 1,
+      orderDate: new Date("2020-01-01")
+    },
+    {
+      id: 3,
+      name: "Product 2 order 2",
+      productId: 2,
+      quantity: 2,
+      status: "done",
+      orderId: 2,
+      orderDate: new Date("2020-01-02")
+    },
+    {
+      id: 4,
+      name: "Product 1 order 3",
+      productId: 1,
+      quantity: 2,
+      status: "processing",
+      orderId: 3,
+      orderDate: new Date("2020-01-03")
+    },
+    {
+      id: 5,
+      name: "Product 3 order 3",
+      productId: 3,
+      quantity: 6,
+      status: "done",
+      orderId: 3,
+      orderDate: new Date("2020-01-03")
+    },
+    {
+      id: 6,
+      name: "Product 1 order 3",
+      productId: 1,
+      quantity: 1,
+      status: "processing",
+      orderId: 3,
+      orderDate: new Date("2020-01-03")
+    },
+    {
+      id: 7,
+      name: "Product 1 order 4",
+      productId: 1,
+      quantity: 1,
+      status: "processing",
+      orderId: 4,
+      orderDate: new Date("2020-01-04")
+    },
+    {
+      id: 8,
+      name: "Product 2 order 4",
+      productId: 2,
+      quantity: 3,
+      status: "done",
+      orderId: 4,
+      orderDate: new Date("2020-01-04")
+    },
+    {
+      id: 9,
+      name: "Product 2 order 5",
+      productId: 2,
+      quantity: 2,
+      status: "processing",
+      orderId: 5,
+      orderDate: new Date("2020-01-05")
+    },
+    {
+      id: 10,
+      name: "Product 3 order 6",
+      productId: 3,
+      quantity: 5,
+      status: "done",
+      orderId: 6,
+      orderDate: new Date("2020-02-01")
+    },
+    {
+      id: 11,
+      name: "Product 3 order 6",
+      productId: 3,
+      quantity: 6,
+      status: "processing",
+      orderId: 6,
+      orderDate: new Date("2020-02-01")
+    },
+    {
+      id: 12,
+      name: "Product 2 order 7",
+      productId: 2,
+      quantity: 2,
+      status: "done",
+      orderId: 7,
+      orderDate: new Date("2020-02-02")
+    },
+    {
+      id: 13,
+      name: "Product 3 order 8",
+      productId: 3,
+      quantity: 2,
+      status: "processing",
+      orderId: 8,
+      orderDate: new Date("2020-02-03")
+    },
+    {
+      id: 14,
+      name: "Product 2 order 8",
+      productId: 2,
+      quantity: 2,
+      status: "processing",
+      orderId: 8,
+      orderDate: new Date("2020-02-03")
+    },
+    {
+      id: 15,
+      name: "Product 1 order 9",
+      productId: 1,
+      quantity: 5,
+      status: "processing",
+      orderId: 9,
+      orderDate: new Date("2020-02-04")
+    },
+    {
+      id: 16,
+      name: "Product 2 order 9",
+      productId: 2,
+      quantity: 2,
+      status: "done",
+      orderId: 9,
+      orderDate: new Date("2020-02-04")
+    },
+    {
+      id: 17,
+      name: "Product 2 order 9",
+      productId: 2,
+      quantity: 3,
+      status: "processing",
+      orderId: 9,
+      orderDate: new Date("2020-02-04")
+    },
+    {
+      id: 18,
+      name: "Product 4 order 9",
+      productId: 4,
+      quantity: 1,
+      status: "done",
+      orderId: 9,
+      orderDate: new Date("2020-02-04")
+    },
+    {
+      id: 19,
+      name: "Product 4 order 10",
+      productId: 4,
+      quantity: 2,
+      status: "processing",
+      orderId: 10,
+      orderDate: new Date("2020-02-05")
+    },
+    {
+      id: 20,
+      name: "Product 1 order 10",
+      productId: 1,
+      quantity: 3,
+      status: "processing",
+      orderId: 10,
+      orderDate: new Date("2020-02-05")
+    }
+  ]
+});
+
 const main = async () => {
   try {
     await prisma.$connect();
@@ -92,6 +277,7 @@ const main = async () => {
     await clear();
     await seedCustomers();
     await seedOrders();
+    await seedProductOrders();
 
     console.log("seeding complete!");
   } catch (e) {

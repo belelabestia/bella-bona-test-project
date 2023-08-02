ebe4c1bed7a13281466f2faeb431bb22601e31ab
Marco Bellezza <jasone.mb@outlook.com>
2023-08-02 15:54:34 +0200

    rename app component files

--- test-project-frontend/src/app.tsx
+++ test-project-frontend/src/app.tsx
@@ -1,6 +1,6 @@
 import React, { Dispatch, createContext, useEffect, useState } from 'react';
 import './app.sass';
-import { Outlet, useNavigate, useNavigation } from 'react-router-dom';
+import { Outlet, useLocation, useNavigate, useNavigation, useParams } from 'react-router-dom';
 import Header from './components/header/header';
 import Footer from './components/footer/footer';
 import { CustomerModel } from "./types/customer-model";
@@ -10,12 +10,24 @@ export const AppContext = createContext(null! as [CustomerModel | null, Dispatch
 
 export const App = () => {
   const [selectedCustomer, selectCustomer] = useState<CustomerModel | null>(null);
+  const { id: customerId } = useParams();
   const { state } = useNavigation();
+  const location = useLocation();
   const navigate = useNavigate();
 
   useEffect(() => {
-    navigate("/home");
-  }, [navigate])
+    if (
+      location.pathname === "/" ||
+      location.pathname === ""
+    ) navigate("/home");
+  }, [navigate, location]);
+
+  useEffect(() => {
+    if (customerId !== undefined)
+      fetch(`/api/customer/${customerId}`)
+        .then(res => res.json())
+        .then(customer => selectCustomer(customer));
+  }, [customerId]);
 
   return <div id="app">
     <AppContext.Provider value={[selectedCustomer, selectCustomer]}>
